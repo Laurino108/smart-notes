3488a178fcecac087f7e8d9acb52e8f209caf431
laurino108 <stapf_laurin@outlook.de>
2023-03-30 11:56:11 +0200

    Save Function
    + Added: Temp Save function

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const AddNote = () => {
+const AddNote = ({ handleAddNote }) => {
     const [noteText, setNoteText] = useState('');
 
     const handleChange = (event) => {
@@ -8,14 +8,17 @@ const AddNote = () => {
     }
 
     const handleSaveClick = () => {
-        
+        if(noteText.trim().length > 0){
+            handleAddNote(noteText);
+            setNoteText(' ');
+        }
     }
 
     return <div className="note new">
         <textarea rows={8} cols={10} placeholder="Type to add a note" onChange={handleChange}></textarea>
         <div className="note-footer">
             <small>20 Remaining</small>
-            <button className="save">Save</button>
+            <button className="save" onClick={handleSaveClick}>Save</button>
         </div>
     </div>;
 }
